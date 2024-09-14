477d660e379a6906ae46eeaa0c503722299fb5fc
Sandro Gerber <sandro-gerber@outlook.com>
2024-09-14 15:36:23 +0200

    Removes unused files

--- js/components/character.js
+++
@@ -1,8 +0,0 @@
-import { Positionable } from '../composables/positionable.js'
-import { Spriteable } from '../composables/spriteable.js'
-
-export class Character {
-  constructor() {
-    Object.assign(this, Positionable(), Spriteable())
-  }
-}
